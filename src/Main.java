fab0c55a29d1afd89dba8aa5676b810ebd761dd4
Bruce Thao <brucethao@Bruces-Mac.local>
2025-02-19 18:40:15 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(system.in);
+        String line = s.nextLine();
+        char c = '$';
+        line.substring(c);
+    }
+}
